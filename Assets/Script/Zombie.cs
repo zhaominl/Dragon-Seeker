d0346840e35b9ea673deec4625e891931a3fe3cc
Tao Wang <taow8@uci.edu>
2018-10-27 16:47:45 -0700

    zombie

--- Assets/Script/Zombie.cs
+++ Assets/Script/Zombie.cs
@@ -4,27 +4,80 @@ using UnityEngine;
 
 public class Zombie :  Enemy{
 
+    //public GameObject player1;
+
+    public float distance;
+    public Transform[] movespots;
+    Transform currentPartolPoint;
+    int currentPartolIndex;
+    bool chase = false;
+
     
 	// Use this for initialization
 	public override void Start () {
-		base.Start();
+        base.Start();
+        currentPartolIndex = 0;
+        currentPartolPoint = movespots[currentPartolIndex];
 	}
 	
 	// Update is called once per frame
 	void Update () {
+
 		
 	}
 
 	private void FixedUpdate(){
 
-        // Zombie chase after the player
+        //Debug.Log(currentPartolIndex+ "currentindex");
+        distance = Vector3.Distance(player.transform.position, transform.position);
 
-        // Change the direction to player
-		Vector3 playerDir = player.transform.position - transform.position;
-		transform.rotation = Quaternion.Euler(0, 0, Mathf.Atan2(playerDir.y, playerDir.x) * Mathf.Rad2Deg - 90);
 
-        // Move toward player
-		transform.Translate(Vector3.Normalize(playerDir) * Time.deltaTime * speed,Space.World);
+        if (distance < 3) {
+            chase = true;
+
+
+        }
+        if (chase)
+        {
+            // Zombie chase after the player
+
+            // Change the direction to player
+            Vector3 playerDir = player.transform.position - transform.position;
+
+            transform.rotation = Quaternion.Euler(0, 0, Mathf.Atan2(playerDir.y, playerDir.x) * Mathf.Rad2Deg - 90);
+            transform.Translate(Vector3.Normalize(playerDir) * Time.deltaTime * speed, Space.World);
+
+        }
+
+        else
+        {
+            transform.Translate(Vector3.up * Time.deltaTime * speed);
+
+
+
+            if (Vector3.Distance(transform.position, currentPartolPoint.position) < .1f)
+            {
+                if (currentPartolIndex + 1 < movespots.Length)
+                {
+
+                    currentPartolIndex++;
+                }
+                else
+                {
+
+                    currentPartolIndex = 0;
+                }
+                currentPartolPoint = movespots[currentPartolIndex];
+            }
+
+            Vector3 patrolPointDir = currentPartolPoint.position - transform.position;
+            float angle = Mathf.Atan2(patrolPointDir.y, patrolPointDir.x) * Mathf.Rad2Deg - 90f;
+
+
+
+            Quaternion q = Quaternion.AngleAxis(angle, Vector3.forward);
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, q, 180f);
+        } 
 	}
 
 	private void OnTriggerEnter(Collider other)
